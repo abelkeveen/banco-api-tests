d6b07943000afb8c92fde12c0b400c69b53f757d
Abelkeveen <aabelkeveenn@gmail.com>
2025-10-01 11:32:32 -0300

    Adicionando a biblioteca .env para variáveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,13 @@
-const request = require('supertest');
-const { expect } = require('chai');
+const request = require('supertest')
+const { expect } = require('chai')
+require('dotenv').config()
 
 describe('Transferências', () =>{
     describe('POST /transferências', () =>{
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async() =>{
             //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            console.log(process.env.BASE_URL)
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -15,7 +17,7 @@ describe('Transferências', () =>{
             
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', 'Bearer ' + token) //`Bearer ${token}`
@@ -33,7 +35,7 @@ describe('Transferências', () =>{
 
         it('Deve retornar sucesso com 422 quando o valor da transferência for abaixo de R$10,00', async() => {
             //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -43,7 +45,7 @@ describe('Transferências', () =>{
             
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', 'Bearer ' + token) //`Bearer ${token}`
